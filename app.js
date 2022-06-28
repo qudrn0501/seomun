31e39b51259adbb85f0901c51c269215a7f0a535
qudrn0501 <qudrn0501@naver.com>
2022-06-28 14:16:20 +0900

    채ㅡ

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 const port = 3000;
 const homes = require('./routes/route.js');
 
+// heroku 꺼지지 않게 하는 구문 or kaffeine.herokuapp.com을 사용
+const http = this.require('https');
+
+setInterval(function () {
+  http.get("https://seomunnightmarket.herokuapp.com/")
+}, 600000);
+
 
 
 
@@ -25,7 +32,7 @@ app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(expressLayouts);
-app.use('/',homes); //use 미들웨어 (중간다리 역할) 등록
+app.use('/', homes); //use 미들웨어 (중간다리 역할) 등록
 
 
 
